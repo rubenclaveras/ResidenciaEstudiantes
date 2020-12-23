d4199cd03ca420e55398b5880c0657e4e46fb667
rubenclaveras <rubenclaveras@opendeusto.es>
2020-12-23 12:57:48 +0100

    Mejora diseño

--- src/residencia/visual/PaginaPrincipal.java
+++ src/residencia/visual/PaginaPrincipal.java
@@ -1,8 +1,11 @@
 package residencia.visual;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -12,21 +15,28 @@ import residencia.clases.SalonComunitario;
 import residencia.clases.Trabajador;
 import residencia.logica.datos.CrearBD;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import java.awt.SystemColor;
+import java.awt.Color;
 
 /**
  * Clase que muestra la ventana principal del programa y direcciona a las siguientes ventanas
  */
 public class PaginaPrincipal extends JFrame {
+	
+	
 
 	private JPanel contentPane;
 	private ArrayList<Estudiante> estudiantes;
 	private ArrayList<Trabajador> empleado;
 	private ArrayList<Habitacion> habitacion;
 	private ArrayList<SalonComunitario> salonComunitario;
+	private JLabel fondo;
 
 	/**
 	 * Launch the application.
@@ -48,6 +58,9 @@ public class PaginaPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public PaginaPrincipal() {
+		setForeground(SystemColor.activeCaptionText);
+		
+		
 		
 		CrearBD base = new CrearBD("ResidenciaEstudiantes.db");
 		base.createLink();
@@ -60,11 +73,14 @@ public class PaginaPrincipal extends JFrame {
 		base.closeLink();
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 412);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		////////poner fondo
+		((JPanel) getContentPane()).setOpaque(false);
+        ImageIcon imagen = new ImageIcon(this.getClass().getResource("imagenes/Imagen1.jpg"));
 		
 		JButton btnEmpleado = new JButton("Empleado");
 		btnEmpleado.addActionListener(new ActionListener() {
@@ -74,7 +90,7 @@ public class PaginaPrincipal extends JFrame {
 				PaginaPrincipal.this.setVisible(false);
 			}
 		});
-		btnEmpleado.setBounds(36, 55, 128, 53);
+		btnEmpleado.setBounds(15, 311, 128, 29);
 		contentPane.add(btnEmpleado);
 		
 		JButton btnEstudiante = new JButton("Estudiante");
@@ -85,7 +101,7 @@ public class PaginaPrincipal extends JFrame {
 				PaginaPrincipal.this.setVisible(false);
 			}
 		});
-		btnEstudiante.setBounds(246, 55, 128, 53);
+		btnEstudiante.setBounds(147, 311, 128, 29);
 		contentPane.add(btnEstudiante);
 		
 		JButton btnSalir = new JButton("Salir");
@@ -94,7 +110,15 @@ public class PaginaPrincipal extends JFrame {
 				PaginaPrincipal.this.dispose();
 			}
 		});
-		btnSalir.setBounds(145, 160, 135, 53);
+		btnSalir.setBounds(428, 311, 135, 29);
 		contentPane.add(btnSalir);
+		
+		fondo = new JLabel();
+		fondo.setVerticalAlignment(SwingConstants.TOP);
+		fondo.setHorizontalAlignment(SwingConstants.RIGHT);
+		fondo.setIcon(imagen);
+		getLayeredPane().add(fondo, JLayeredPane.FRAME_CONTENT_LAYER);
+		fondo.setBounds(-24, -16, 600, 412);
+		getContentPane().add(fondo, BorderLayout.CENTER);
 	}
 }
