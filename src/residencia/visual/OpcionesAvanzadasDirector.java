52f4696f0621ee04b5675d21b454b87c89878421
rubenclaveras <rubenclaveras@opendeusto.es>
2021-01-11 19:04:33 +0100

    Implementacion quicksort mejorada

--- src/residencia/visual/OpcionesAvanzadasDirector.java
+++ src/residencia/visual/OpcionesAvanzadasDirector.java
@@ -45,22 +45,35 @@ public class OpcionesAvanzadasDirector extends JFrame {
 		((JPanel) getContentPane()).setOpaque(false);
 		ImageIcon imagen = new ImageIcon(this.getClass().getResource("imagenes/Director.jpg"));
 		
-		JButton btnNewButton = new JButton("Estudiantes con cuotas más altas");
+		JButton btnNewButton = new JButton("Estudiantes ordenados por cuota anual");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				ArrayList <Integer> listaCuotas = null;
-				for (Estudiante a: estudiantes){
-					listaCuotas.add(a.cuotaAnual);
+				
+				
+				ArrayList <Integer> listaCuotas = new ArrayList <Integer> ();
+				for (Estudiante a: estudiantes ){
+					listaCuotas.add(a.getCuotaAnual());
 				}
-				int izq = 0;
-				int der = (listaCuotas.size()-1);
-				ArrayList <Integer> cuotasOrdenadas = quicksortEstudiantes(listaCuotas, izq, der);
-				ArrayList<Estudiante> estudiantesOrdenados = null;
-				for (int i=0;i<cuotasOrdenadas.size()-1; i++){
+				 
+				
+				 
+				ArrayList <Integer> cuotasOrdenadas = quicksortOrden(listaCuotas);
+				ArrayList<String> estudiantesOrdenados = new ArrayList<String> () ;
+				ArrayList<String> estudiantesAuxiliar = new ArrayList<String> () ;
+				for (Estudiante a: estudiantes){
+					estudiantesAuxiliar.add(a.getNombre());
+				}
+				//Todo esto se hace para: crear una lista auxiliar con los nombres de todos los estudiantes, y a continuacion se van comparando los estudiantes
+				// con las cuotas ordenadas. Si coincide, se añade a una lista con los nombres de los estudiantes ordenados, y se elimina de la lista auxiliar con los nombres.
+				// Se hace de esta manera para así evitar poner dos veces a un estudiante cuando hay mas de uno que paga anualmente la misma cantidad.
+				for (int i= 0;i < (cuotasOrdenadas.size())-1; i++){
 					for (Estudiante a: estudiantes){
 						if (a.getCuotaAnual()==cuotasOrdenadas.get(i)){
-							estudiantesOrdenados.add(a);
-							break;
+							if (estudiantesAuxiliar.contains(a.getNombre())){
+								estudiantesOrdenados.add(a.getNombre()+ " " + cuotasOrdenadas.get(i) + "€");
+								estudiantesAuxiliar.remove(a.getNombre());
+							}
+							
 						}
 					}
 					
@@ -72,13 +85,14 @@ public class OpcionesAvanzadasDirector extends JFrame {
 						estudiantesOrdenados.get(4)+"\n" +
 						estudiantesOrdenados.get(5)+"\n" +
 						estudiantesOrdenados.get(6)+"\n");
+				
 			}
 		});
-		btnNewButton.setBounds(15, 16, 277, 52);
+		btnNewButton.setBounds(15, 16, 313, 52);
 		contentPane.add(btnNewButton);
 		
-		JButton btnNewButton2 = new JButton("Empleados con salarios mas altos");
-		btnNewButton2.setBounds(15, 84, 277, 52);
+		JButton btnNewButton2 = new JButton("Empleados ordenados por salario anual");
+		btnNewButton2.setBounds(15, 84, 313, 52);
 		contentPane.add(btnNewButton2);
 		
 		JButton btnVolver = new JButton("Salir");
@@ -102,37 +116,64 @@ public class OpcionesAvanzadasDirector extends JFrame {
 		getContentPane().add(fondo, BorderLayout.CENTER);
 	
 	}
-	public ArrayList<Integer>  quicksortEstudiantes (ArrayList <Integer> listaCuotas, int izquierda, int derecha){
+	/**
+	 * Metodo que nos devolverá la lista ordenada
+	 * @param listaCuotas lista con todas las cuotas
+	 * @return lista ordenada
+	 */
+	public ArrayList<Integer> quicksortOrden (ArrayList<Integer> listaCuotas){
+		return quicksort1(listaCuotas);
+	}
+	/**
+	 *  Metodo que le pasa a quicksort los valores que necesita
+	 * @param listaCuotas lista con todas las cuotas
+	 * @return lista ordenada
+	 */
+	public ArrayList<Integer> quicksort1 (ArrayList<Integer> listaCuotas){
+		return quicksort2 (listaCuotas, 0, (listaCuotas.size())-1);
+	}
+	/**
+	 * Metodo para hacer el quicksort y ordenar la lista de las cuotas. Se usa como pivote el elemento de la izquierda.
+	 * @param listaCuotas lista con todas las cuotas
+	 * @param izq el puntero que señala por la izquierda
+	 * @param der el puntero que señala por la derecha
+	 * @return lista ordenada
+	 */
+	public ArrayList<Integer> quicksort2 (ArrayList<Integer> listaCuotas, int izq, int der){
 		
-		int pivote = listaCuotas.get(izquierda);
-		int x = izquierda;
-		int y = derecha;
-		int auxiliar;
+		if (izq>=der)
+			return listaCuotas;
 		
+		int i = izq, d = der;
 		
-		while (x<y){
-			while (listaCuotas.get(x)<= pivote && x<y){
-				x++;
-			}
-			while (listaCuotas.get(y) > pivote){
-				y--;
-			}
-			if (x<y){
-				auxiliar = listaCuotas.get(x);
-				listaCuotas.set(x, listaCuotas.get(y));
-				listaCuotas.set(y, auxiliar);
-			}
+		if (izq!=der){
+			int pivote;
+			int aux;
+			pivote = izq;
+			while (izq != der){
+				while (listaCuotas.get(der) >= listaCuotas.get(pivote) && izq < der)
+					der--;
+				
+				while (listaCuotas.get(izq) < listaCuotas.get(pivote) && izq < der)
+					izq++;
+				
+			
+			if (der != izq){
+				aux = listaCuotas.get(der);
+				listaCuotas.set(der, listaCuotas.get(izq));
+				listaCuotas.set(izq, aux);
 		}
-		listaCuotas.set(izquierda, listaCuotas.get(y));
-		listaCuotas.set(y, pivote);
-		
-		if (izquierda < y-1){
-			quicksortEstudiantes(listaCuotas, izquierda, y-1);
 		}
-		if (y+1 < derecha){
-			quicksortEstudiantes(listaCuotas, y+1, derecha);
+		if (izq == der){
+			quicksort2 (listaCuotas, i, izq-1);
+			quicksort2 (listaCuotas, izq+1, d);
 		}
+		}
+		else
+			return listaCuotas;
 		
 		return listaCuotas;
 	}
+	
+	 
 }
